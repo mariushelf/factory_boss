91cf22a734b0ccb7f20020281439f03786cca94e
Marius Helf <helfsmarius@gmail.com>
2021-05-20 23:39:18 +0200

    experiments with lard

---
+++ factory_boss/grammar.py
@@ -0,0 +1,37 @@
+value_grammar = r"""
+start : expression
+expression : (reference | literal)+
+reference.10 : "$" _reference_body
+          | "${" _WS* _reference_body _WS* "}"
+_reference_body : ref_element ("." ref_element)*
+ref_element.10 : element_name [element_access]
+element_name.10 : CNAME
+element_access.10 :  "[" _WS* slice _WS* "]"
+slice : index
+index : INT | SIGNED_INT
+
+literal.-1: (/[^$\\]+/ | _escape DOLLAR | _escaped_backslash )+
+DOLLAR: "$"
+_escape : _BACKSLASH
+_escaped_backslash : _BACKSLASH BACKSLASH
+BACKSLASH : "\\"
+_BACKSLASH : "\\"
+
+_WS: WS
+
+%import common.WS
+%import common.NUMBER
+%import common.SIGNED_NUMBER
+%import common.INT
+%import common.SIGNED_INT
+%import common.CNAME
+"""
+
+if __name__ == "__main__":
+    from lark import Lark
+
+    parser = Lark(value_grammar, parser="earley")
+
+    code = r"$a[-56 ].b${ this.is.a.ref}fgds gfd $FIRST.x[1] \$test \\$test2 \\\$test3 \\\\$test4"
+    print(parser.parse(code))
+    print(parser.parse(code).pretty())
